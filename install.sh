f6ad2fa5eb777c7707d0b72269b186b3b0f1349d
nathan <nathan@rizzo.mrsv.co>
2019-02-20 10:57:29 -0800

    update install script to kinda work

--- install.sh
+++ install.sh
@@ -1,2 +1,22 @@
-ln -s ~/dotfiles/sshconfig ~/.ssh/config
-mkdir ~/bin
+#!/bin/bash
+
+set -x
+set -e
+
+if [[ ! -f ~/.ssh/config ]]; then
+   ln -s ~/dotfiles/sshconfig ~/.ssh/config
+fi
+
+if [[ ! -d ~/bin ]]; then
+    mkdir -p ~/bin
+fi
+
+if [[ -f ~/.bashrc ]]
+   if grep --quiet dotfiles/.bashrc; then
+   else
+       echo "source ~/dotfiles/.bashrc" >> ~/.bashrc
+   fi
+fi
+
+   
+
